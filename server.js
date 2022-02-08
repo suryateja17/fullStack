84ef12475ff25ae50889e5223965fe8f380932ec
suryateja17 <surya.tejapothureddy@meraki.net>
2022-02-08 14:24:54 +0000

    New Blog

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const articleRouter = require('./routes/articles')
 const Article = require('./models/article')
@@ -6,10 +10,13 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect(process.env.CLOUD_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
+const db = mongoose.connection
+db.once('error', (error) => console.log('**Connection Error!' + error))
+db.once('open', () => console.log('**Connection established'))
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -26,6 +33,6 @@ app.get('/', async function (req, res) {
 
 app.use('/articles', articleRouter)
 
-app.listen(5000, function () {
+app.listen(process.env.PORT || 5000, function () {
   console.log('New Blog listening on port 5000!')
 })
